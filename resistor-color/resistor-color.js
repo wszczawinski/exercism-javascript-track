a1055feec609216d0dea217f558717a09dcc4b11
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-09-06 21:34:32 +0200

    feat(3): check index of value in array

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,17 @@
+export const colorCode = (color) => {
+  let index = COLORS.indexOf(color);
+  return index;
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
